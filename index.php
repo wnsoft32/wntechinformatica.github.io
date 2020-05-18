4b96a57a5f63ce9861c12ea94f6e7cacd86d74d2
wnsoft32 <65573293+wnsoft32@users.noreply.github.com>
2020-05-18 20:21:17 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<title>WNTech Informática - Baixe Nosso App do Cliente</title> 
+
+<head> </head> 
+
+<body>
+
+<h1>Seja Bem-vindo.</h1> 
+<h2> Baixe nosso Aplicativo </h2> 
+<p>Disponível para </p> <a href="">Android</a> ou  <a href="">Windows.</a>
+
+</body> 
+<style> </style> 
